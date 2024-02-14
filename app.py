f5df57d47aa512ebdcb31b7fe7b92100125bac0e
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2024-02-14 16:47:21 +0100

    Changed schema, test implementation for /api/chats

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import flask_login
 from flask import Flask, request
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from hash import hash_password, check_password
 from flask_login import login_user, UserMixin, LoginManager, logout_user, login_required
+from sqlalchemy import ForeignKey
+
 
 app = Flask(__name__)
 
@@ -22,6 +25,8 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, unique=False, nullable=False)
@@ -32,43 +37,65 @@ class User(db.Model, UserMixin):
 
 
 class ChatMember(db.Model):
+    __tablename__ = 'chatmember'
+
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-    groupchat_id = db.Column(db.Integer, unique=False, nullable=False)
-    user_id = db.Column(db.Integer, unique=False, nullable=False)
+    groupchat_id = db.Column(db.Integer, ForeignKey('groupchat.id'), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'), unique=False, nullable=False)
     nickname = db.Column(db.String, unique=False, nullable=False)
     isAdmin = db.Column(db.Boolean, unique=False, nullable=False)
     isRemoved = db.Column(db.Boolean, unique=False, nullable=False)
 
+    groupchat = db.relationship('GroupChat')
+    user = db.relationship('User')
+
 
 class GroupChat(db.Model):
+    __tablename__ = 'groupchat'
+
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     name = db.Column(db.String, unique=False, nullable=False)
 
 
 class GroupMessage(db.Model):
+    __tablename__ = 'groupmessage'
+
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-    member_id = db.Column(db.Integer, unique=False, nullable=False)
-    groupchat_id = db.Column(db.Integer, unique=False, nullable=False)
+    member_id = db.Column(db.Integer, ForeignKey('chatmember.id'), unique=False, nullable=False)
+    groupchat_id = db.Column(db.Integer, ForeignKey('groupchat.id'), unique=False, nullable=False)
     content = db.Column(db.String, unique=False, nullable=False)
     isDeleted = db.Column(db.Boolean, unique=False, nullable=False)
     timestamp = db.Column(db.DateTime, unique=False, nullable=False)
     attachment = db.Column(db.LargeBinary, unique=False, nullable=True)
+
+    member = db.relationship('ChatMember')
+    groupchat = db.relationship('GroupChat')
 
 
 class PrivateChat(db.Model):
+    __tablename__ = 'privatechat'
+
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-    from_id = db.Column(db.Integer, unique=False, nullable=False)
-    to_id = db.Column(db.Integer, unique=False, nullable=False)
+    from_id = db.Column(db.Integer, ForeignKey('user.id'), unique=False, nullable=False)
+    to_id = db.Column(db.Integer, ForeignKey('user.id'), unique=False, nullable=False)
+
+    from_user = db.relationship("User", foreign_keys=[from_id])
+    to_user = db.relationship("User", foreign_keys=[to_id])
 
 
 class PrivateMessage(db.Model):
+    __tablename__ = 'privatemessage'
+
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-    user_id = db.Column(db.Integer, unique=False, nullable=False)
-    privatechat_id = db.Column(db.Integer, unique=False, nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey("user.id"), unique=False, nullable=False)
+    privatechat_id = db.Column(db.Integer, ForeignKey('privatechat.id'), unique=False, nullable=False)
     content = db.Column(db.String, unique=False, nullable=False)
     isDeleted = db.Column(db.Boolean, unique=False, nullable=False)
     timestamp = db.Column(db.DateTime, unique=False, nullable=False)
     attachment = db.Column(db.LargeBinary, unique=False, nullable=True)
+
+    user = db.relationship("User")
+    privateChat = db.relationship("PrivateChat")
 
 
 with app.app_context():
@@ -76,7 +103,7 @@ with app.app_context():
 
 
 @app.route('/api', methods=["GET", "POST"])
-def hello_world():  # put application's code here
+def hello_world():  # TODO: List api routes!
     return jsonify(
         response="API works correctly",
     )
@@ -146,6 +173,19 @@ def logout():
         )
 
 
+@app.route('/api/chats', methods=["GET"])
+@login_required
+def get_chats():
+    result = db.session.query(GroupChat.id, GroupChat.name).select_from(GroupChat).join(ChatMember).filter(ChatMember.user_id == flask_login.current_user.id).all()
+    chats = []
+    for row in result:
+        chats.append((row.id, row.name))
+    return jsonify(
+        chats=chats
+    )
+
+
+
 if __name__ == '__main__':
     app.run(ssl_context='adhoc')
 
@@ -154,12 +194,14 @@ if __name__ == '__main__':
 # zrobione POST /api/login
 # zrobione POST /api/register
 
+# POST /api/userlist/
+# POST /api/user/{id} <- zwraca wiadomosci z danym użytkownikiem
+# POST /api/user/{id}/send
 # POST /api/chats/create
-# GET  /api/chats/
-# GET  /api/chats/{id}/
+# POST /api/chats/{id}/add_user
+# GET  /api/chats/ <- wyswietla czaty użytkownika
+# GET  /api/chats/{id}/ <- wyswietla wiadomosci na czacie
 # POST /api/chats/{id}/message/send
-# POST /api/chats/{id}/message/delete
-# POST /api/chats/{id}/message/edit
 
 
 
